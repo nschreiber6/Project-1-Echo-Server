99f73b98b81851d7f9581e8667f3535c2cdd62a9
Nadia Schreiber <nschreiber@mymail.mines.edu>
2021-09-01 17:11:19 -0600

    fixed errors

--- echo_s.cc
+++ echo_s.cc
@@ -21,7 +21,7 @@ int processConnection(int sockFd) {
     // Hint - don't forget to zero out the buffer each time you use it.
     //
     char filename[] = "echoFile.txt";
-    int fd = open(filename, O_APPEND | O_RDWR | O_EXCL);
+    int fd = open(filename, O_APPEND | O_RDWR | O_EXCL, 0777);
     if (fd == -1){
       std::cout << "open failed" << strerror(errno) << std::endl;
       exit(-1);
