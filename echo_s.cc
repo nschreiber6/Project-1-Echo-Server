1ce0a21273e709d5270179e3dc12d1efff72bf7a
Nadia Schreiber <nschreiber@mymail.mines.edu>
2021-09-01 20:28:35 -0600

    fixed errors

--- echo_s.cc
+++ echo_s.cc
@@ -28,8 +28,8 @@ int processConnection(int sockFd) {
     //
 
     char buffer[1024] = {0};
-    int bytesRead = 0;
-    if (bytesRead = read(sockFd,buffer,1024) < 0 ) {
+    int bytesRead = read(sockFd,buffer,1024);
+    if (bytesRead < 0 ) {
       std::cout << "read failed" << strerror(errno) << std::endl;
       exit(-1);
     }
